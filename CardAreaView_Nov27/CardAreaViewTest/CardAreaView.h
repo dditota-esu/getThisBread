0eefa331b5c8c476f0b32cabf31e74277b5ac225
aharris-esu <72219012+aharris-esu@users.noreply.github.com>
2020-11-27 20:27:00 -0500

    CardAreaView that was worked on with Hofmiester

---
+++ CardAreaView_Nov27/CardAreaViewTest/CardAreaView.h
@@ -0,0 +1,14 @@
+#if !defined CARDAREAVIEW_H
+#define CARDAREAVIEW_H
+
+class CardAreaView
+{
+private:
+
+public:
+	CardAreaView(int x, int y, int xExtent, int yExtent, IListOfList* list2d, ICardList* list1d,  HINSTANCE hInst);
+
+};
+#endif
+
+//This class creates CardView so --> CardView one = new CardAView();
